503462ece68064cec1df4fa24a2bfce649656822
Mourat <mourat@boutry.me>
2023-01-27 16:28:50 +0100

    Added player movement. Player size changed

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,25 @@
+using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+    [SerializeField] private CharacterController controller;
+    [SerializeField] private float speed;
+
+    private void Reset()
+    {
+        speed = 12f;
+    }
+
+    private void Update()
+    {
+        var x = Input.GetAxis("Horizontal");
+        var z = Input.GetAxis("Vertical");
+        
+        var objTransform = transform;
+        var move = objTransform.right * x + objTransform.forward * z;
+
+        var moveSpeed = move * speed;
+        controller.Move(moveSpeed * Time.deltaTime);
+    }
 }
